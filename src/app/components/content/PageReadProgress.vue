b84dfb907a2570b84fe282f6dcbe5a93db1814c2
Jeroen <jeroen.bach@gmail.com>
2024-12-16 22:26:25 +0100

    Article 2
    - sorted the articles on date desc
    - added totalProgress and fixed tests
    - added PageReadProgress.vue for in the articles to show the current progress
    - removed background of info NotificationMessage

---
+++ src/app/components/content/PageReadProgress.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+import { useReadProgressTracking } from "@/composables/useReadProgressTracking";
+import NotificationMessage from "@/components/NotificationMessage.vue";
+
+interface Props {
+  wordCount?: number;
+}
+
+const { wordCount } = defineProps<Props>();
+
+const { totalProgress, timeSpent, timeSpentPercentage, scrollPercentage } =
+  useReadProgressTracking({ wordCount });
+</script>
+
+<template>
+  <NotificationMessage severity="info">
+    <div class="flex flex-col">
+      <span>Word count: {{ wordCount }}</span>
+      <span>Time: {{ timeSpentPercentage?.toFixed(1) }}%</span>
+      <span>Scroll: {{ scrollPercentage?.toFixed(1) }}%</span>
+      <span>Total: {{ totalProgress }}</span>
+    </div>
+  </NotificationMessage>
+</template>
